6cb3f1b1db9e638d35edb0e107a43b65b43e167f
Oleh Karnaukhov <oleh.karnaukhov@play.works>
2025-05-12 17:23:20 +0200

    Added test scripts

---
+++ 837edbbd-830e-456a-b07b-e68b4fe21fc3/test.dart
@@ -0,0 +1,34 @@
+import 'package:game/SaveManager.dart'; 
+import 'package:game/ScriptManager.dart'; 
+import 'package:game/test2.dart'; 
+
+var sv = getSaveManager(); 
+var scr = getScriptManager(); 
+
+void myFunc(int count, String str) { 
+	print("TEST MANAGERS: " + count.toString() + "; " + str); 
+	
+	// Test saveManager
+	final score = sv.getValue("score"); 
+	print(score);
+	sv.setValue("score", 18, isSave: true); 
+	final newScore = sv.getValue("score"); 
+	print(newScore);
+	final currentTheme = sv.getSettingsValue("currentTheme"); 
+	print(currentTheme);
+	sv.setSettingsValue("currentTheme", "space");
+	final newCurrentTheme = sv.getSettingsValue("currentTheme"); 
+	print(newCurrentTheme);
+	sv.clearData();
+	final zeroScore = sv.getValue("score"); 
+	print(zeroScore);
+	
+	// Test local functions
+	print(myLocalFunc(count, str));
+	print(myAnotherLocalFunc(count, str));
+}
+
+int myLocalFunc(int count, String str) {
+	print("LOCAL FUNCTION: " + count.toString() + "; " + str); 
+	return 5;
+}
---
+++ 837edbbd-830e-456a-b07b-e68b4fe21fc3/test2.dart
@@ -0,0 +1,4 @@
+int myAnotherLocalFunc(int count, String str) {
+	print("ANOTHER FILE LOCAL FUNCTION: " + count.toString() + "; " + str); 
+	return 5;
+}
